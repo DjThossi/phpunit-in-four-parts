ac42085a1a40cb7823ab0fb438444531e68d52db
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2019-03-09 13:20:20 -0500

    Simplifying example code

--- src/Factory.php
+++ src/Factory.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 namespace DjThossi\PHPUnit;
 
 use DjThossi\PHPUnit\Reader\CardReader;
-use DjThossi\PHPUnit\Reader\ColorReader;
 use DjThossi\PHPUnit\Reader\DummyCardReader;
-use DjThossi\PHPUnit\Reader\DummyColorReader;
-use DjThossi\PHPUnit\Reader\DummyOptionReader;
-use DjThossi\PHPUnit\Reader\OptionReader;
 
 class Factory
 {
@@ -19,19 +15,6 @@ class Factory
 
     private function createCardReader(): CardReader
     {
-        return new DummyCardReader(
-            $this->createColorReader(),
-            $this->createOptionsReader()
-        );
-    }
-
-    private function createColorReader(): ColorReader
-    {
-        return new DummyColorReader();
-    }
-
-    private function createOptionsReader(): OptionReader
-    {
-        return new DummyOptionReader();
+        return new DummyCardReader();
     }
 }
--- src/Reader/DummyCardReader.php
+++ src/Reader/DummyCardReader.php
@@ -3,40 +3,35 @@
 declare(strict_types=1);
 namespace DjThossi\PHPUnit\Reader;
 
+use DjThossi\PHPUnit\Collection\ColorCollection;
+use DjThossi\PHPUnit\Collection\OptionCollection;
 use DjThossi\PHPUnit\Domain\Card;
+use DjThossi\PHPUnit\Domain\Color;
 use DjThossi\PHPUnit\Domain\Format;
+use DjThossi\PHPUnit\Domain\Option;
 use DjThossi\PHPUnit\Domain\Sku;
 use DjThossi\PHPUnit\Exception\SkuNotFoundException;
 
 class DummyCardReader implements CardReader
 {
-    /**
-     * @var ColorReader
-     */
-    private $colorReader;
-
-    /**
-     * @var OptionReader
-     */
-    private $optionReader;
-
-    public function __construct(ColorReader $colorReader, OptionReader $optionReader)
-    {
-        $this->colorReader = $colorReader;
-        $this->optionReader = $optionReader;
-    }
-
     public function getCard(Sku $sku): Card
     {
         if ($sku->asString() !== 'KAM11GG') {
             throw new SkuNotFoundException('Provided SKU not found');
         }
 
+        $colors = new ColorCollection();
+        $colors->addColor(new Color('C05'));
+
+        $options = new OptionCollection();
+        $options->addOption(new Option('D01'));
+        $options->addOption(new Option('V05'));
+
         return new Card(
             new Sku('KAM11GG'),
             new Format('F020'),
-            $this->colorReader->getColors(),
-            $this->optionReader->getOptions()
+            $colors,
+            $options
         );
     }
 }
