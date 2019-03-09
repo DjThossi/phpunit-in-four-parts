151d94170f5ca3d6a4029a92ec0cdf1cee5d4dbf
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2019-03-09 09:26:32 -0500

    add missing tests

---
+++ tests/Integration/RequestTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DjThossi\PHPUnit\IntegrationTest;
+
+use DjThossi\PHPUnit\Exception\MissingGetParameterException;
+use DjThossi\PHPUnit\Request;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DjThossi\PHPUnit\Request
+ */
+class RequestTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testCanCreateFromSuperGlobals(): void
+    {
+        $_GET['sku'] = 'My test sku';
+        $request = Request::fromSuperGlobals();
+
+        $this->assertInstanceOf(Request::class, $request);
+    }
+
+    public function testFromSuperGlobalsThrowsExceptionOnMissingParameter(): void
+    {
+        $this->expectException(MissingGetParameterException::class);
+
+        Request::fromSuperGlobals();
+    }
+}
