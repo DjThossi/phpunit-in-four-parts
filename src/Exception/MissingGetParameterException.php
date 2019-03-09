ac5585a6781487ab1faf91314e4e2a2a96195066
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2019-03-09 07:56:48 -0500

    Introduce Request class

---
+++ src/Exception/MissingGetParameterException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace DjThossi\PHPUnit\Exception;
+
+use Exception;
+
+class MissingGetParameterException extends Exception
+{
+
+}
---
+++ src/Request.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace DjThossi\PHPUnit;
+
+use DjThossi\PHPUnit\Domain\Sku;
+use DjThossi\PHPUnit\Exception\MissingGetParameterException;
+
+class Request
+{
+    /**
+     * @var Sku
+     */
+    private $sku;
+
+    public function __construct(Sku $sku)
+    {
+        $this->sku = $sku;
+    }
+
+    public static function fromSuperGlobals(): self
+    {
+        self::ensureSkuIsInGetVariable();
+        return new self(new Sku($_GET['sku']));
+    }
+
+    public function getSku(): Sku
+    {
+        return $this->sku;
+    }
+
+    private static function ensureSkuIsInGetVariable(): void
+    {
+        if (!isset($_GET['sku'])) {
+            throw new MissingGetParameterException('Missing field sku in $_GET');
+        }
+    }
+}
